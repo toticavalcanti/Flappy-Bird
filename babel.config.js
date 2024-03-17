fcd88aebcdafdb542076bfe654c840758cc5b8d6
toticavalcanti <toticavalcanti@hotmail.com>
2024-03-17 19:27:59 -0300

    Atualiza package-lock.json para sincronizar com package.json

---
+++ babel.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  presets: [
+    "@babel/preset-env",
+    "@babel/preset-react",
+  ],
+};module.exports = {
+  presets: [
+    "@babel/preset-env",
+    "@babel/preset-react",
+  ],
+  plugins: [
+    "@babel/plugin-transform-private-methods",
+  ],
+};
